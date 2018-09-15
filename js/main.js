b526efe065afda8611576ac2458b18e16530cf64
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2018-09-15 00:41:47 -0300

    add logo

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ for(i=0; i<labels.length; i++){
     labels[i].onclick = function(){
         let color = document.querySelector(".rd-tabs:checked ~ .content").getAttribute("data-color");
         document.querySelector(".main").style.backgroundColor = color;    
-        document.querySelector(".face-label").style.borderColor = color;    
+        document.querySelector(".logo-label").style.borderColor = color;    
     }
 }

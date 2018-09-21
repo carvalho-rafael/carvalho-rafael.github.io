7e34a808f0126944dc4d8b70cfef497fa65a5ceb
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2018-09-21 01:23:59 -0300

    Change layout

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let labels = document.querySelectorAll(".rd-tabs"), i;
-
-for(i=0; i<labels.length; i++){
-    labels[i].onclick = function(){
-        let color = document.querySelector(".rd-tabs:checked ~ .content").getAttribute("data-color");
-        document.querySelector(".main").style.backgroundColor = color;    
-        document.querySelector(".logo-label").style.borderColor = color;    
-    }
-}

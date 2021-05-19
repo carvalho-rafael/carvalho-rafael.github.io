ac1b14fcaad09c61e0c212337a108d508200ef80
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-05-19 11:25:15 -0300

    setup typescript and google Analytics

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import { HomeContainer } from '../styles/pages/index'
+export default function Home() {
+  return (
+    <HomeContainer>
+      <h1>Olá  nova pagina pessoal</h1>
+    </HomeContainer>
+  )
+}
---
+++ styles/pages/index.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const HomeContainer= styled.main`
+    background: blue;
+    font-family: 'Courier New', Courier, monospace;
+    color: #121212;
+`
